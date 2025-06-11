312d976fb74efe6212d6ddead0968520dd218325
Melnichuk2 <105264214+Melnichuk2@users.noreply.github.com>
2025-06-11 20:03:22 +0300

    1.2.5
    Update

--- server.js
+++ server.js
@@ -5,30 +5,30 @@ const cors = require('cors');
 const app = express();
 const PORT = 3000;
 
-// Используем CORS для работы с запросами с других доменов (если нужно)
+
 app.use(cors());
 
-// Статическая папка для файлов
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Данные о планетах
+
 const planets = {
-    Меркурій: { semiMajorAxis: 0.39, eccentricity: 0.2056 },
-    Венера: { semiMajorAxis: 0.72, eccentricity: 0.0067 },
-    Земля: { semiMajorAxis: 1.0, eccentricity: 0.0167 },
-    Марс: { semiMajorAxis: 1.52, eccentricity: 0.0934 },
-    Юпітер: { semiMajorAxis: 5.2, eccentricity: 0.0489 },
-    Сатурн: { semiMajorAxis: 9.58, eccentricity: 0.0557 },
-    Уран: { semiMajorAxis: 19.2, eccentricity: 0.0444 },
-    Нептун: { semiMajorAxis: 30.05, eccentricity: 0.011 }
+    Меркурій: { semiMajorAxis: 0.39, eccentricity: 0.2056, image: '/images/mercury.png' },
+    Венера: { semiMajorAxis: 0.72, eccentricity: 0.0067, image: '/images/venus.png' },
+    Земля: { semiMajorAxis: 1.0, eccentricity: 0.0167, image: '/images/earth.png' },
+    Марс: { semiMajorAxis: 1.52, eccentricity: 0.0934, image: '/images/mars.png' },
+    Юпітер: { semiMajorAxis: 5.2, eccentricity: 0.0489, image: '/images/jupiter.png' },
+    Сатурн: { semiMajorAxis: 9.58, eccentricity: 0.0557, image: '/images/saturn.png' },
+    Уран: { semiMajorAxis: 19.2, eccentricity: 0.0444, image: '/images/uranus.png' },
+    Нептун: { semiMajorAxis: 30.05, eccentricity: 0.011, image: '/images/neptune.png' }
 };
 
-// API для получения данных о планетах
+
 app.get('/api/planets', (req, res) => {
     res.json(planets);
 });
 
-// Запуск сервера
+
 app.listen(PORT, () => {
     console.log(`Сервер запущен: http://localhost:${PORT}`);
 });
